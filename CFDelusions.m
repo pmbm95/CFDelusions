aa371dd653463b4ab4803af9575c3cc2a0faa1bb
pmbm95 <38461119+pmbm95@users.noreply.github.com>
2018-04-22 16:07:24 +0100

    error

--- CFDelusions.m
+++ CFDelusions.m
@@ -1,10 +1,11 @@
 clc
 clear all
+close all
 
 dominio_x = [-0.5 0.5];
 dominio_y = [-0.5 0.5];
-n_nodes_x = 20;
-n_nodes_y = 10;
+n_nodes_x = 100;
+n_nodes_y = 100;
 
 dx = (dominio_x(2) - dominio_x(1) ) / n_nodes_x;
 dy = (dominio_y(2) - dominio_y(1) ) / n_nodes_y;
@@ -16,14 +17,15 @@ centro_y = (dominio_y(1)+dy/2) : dy : (dominio_y(2)-dy/2);
 
 %plot(x_nodes, y_nodes, 'k.')
 
-A = zeros(n_nodes_y*n_nodes_x);
-B = zeros(n_nodes_y*n_nodes_x, 1);
+A = sparse(n_nodes_y*n_nodes_x, n_nodes_y*n_nodes_x);
+B = sparse(n_nodes_y*n_nodes_x, 1);
 
 %--------- Coeff de difusao ----------
 cd = 1;
 
 i = 1 ;% Vertical Counter
 j = 1; % Horizontal counter
+
 
 S_boundary_n = false;
 N_boundary_n = false;
@@ -41,7 +43,9 @@ for k=1:n_nodes_y*n_nodes_x
     if j == 1 % Boundary
         A(k,k) = A(k,k) - 3.5*cd*dy/dx;
         A(k,k+1) = A(k,k+1) + 0.5*cd*dy/dx;
-        B(k) = B(k)- 3*cd*dy/dx*exact(centro_x(j)-dx/2, centro_y(i));
+        B(k) = B(k)- 3*cd*dy/dx*exact(centro_x(j)-(dx/2), centro_y(i));
+        %centro_x(j)-(dx/2)
+        %centro_y(i)
     else % Center
         A(k,k) = A(k,k) - cd*dy/dx;
         A(k,k-1) = A(k,k-1) + cd*dy/dx;        
@@ -71,18 +75,11 @@ for k=1:n_nodes_y*n_nodes_x
     if i == n_nodes_y % Boundary
         A(k,k) = A(k,k) - 3.5*cd*dx/dy;
         A(k,k-n_nodes_x) = A(k,k-n_nodes_x) + 0.5*cd*dy/dy;
-        B(k) = B(k) - 3*cd*dx/dy*exact(centro_x(j), centro_y(i)+dy);
+        B(k) = B(k) - 3*cd*dx/dy*exact(centro_x(j), centro_y(i)+(dy/2));
     else % Center
         A(k,k) = A(k,k) - cd*dx/dy;
         A(k,k+n_nodes_x) = A(k,k+n_nodes_x) + cd*dx/dy;
     end
-    
-%     % Middle
-%     A(k,k) = A(k,k) - 2 *cd*(dx/dy + dy/dx);
-%     A(k,k-1) = A(k,k-1) + cd*dy/dx;
-%     A(k,k+1) = A(k,k+1) + cd*dy/dx;
-%     A(k,k+n_nodes_x) = A(k,k+n_nodes_x) + cd*dx/dy;
-%     A(k,k-n_nodes_x) = A(k,k-n_nodes_x) + cd*dx/dy;
     
     B(k) = B(k) - source(centro_x(j), centro_y(i));
     
@@ -94,4 +91,18 @@ for k=1:n_nodes_y*n_nodes_x
     end
 end
 
-fprintf('lol')
+
+U = A\B;
+
+U = vec2mat(U,n_nodes_x);
+pcolor(x_nodes, y_nodes, U)
+colorbar;
+U_exact = exact(x_nodes, y_nodes);
+
+figure
+Error = U_exact - U;
+pcolor(x_nodes, y_nodes, U_exact);
+colorbar;
+
+
+%fprintf('lol')
