efc2f65ad63ddcbf3398f51a07b0f668ac3d45df
pmbm95 <38461119+pmbm95@users.noreply.github.com>
2018-04-22 11:49:37 +0100

    dsddf

--- CFDelusions.m
+++ CFDelusions.m
@@ -1,3 +1,6 @@
+clc
+clear all
+
 dominio_x = [-0.5 0.5];
 dominio_y = [-0.5 0.5];
 n_nodes_x = 20;
@@ -11,7 +14,7 @@ centro_y = (dominio_y(1)+dy/2) : dy : (dominio_y(2)-dy/2);
 
 [x_nodes y_nodes] = meshgrid(centro_x, centro_y);
 
-plot(x_nodes, y_nodes, 'k.')
+%plot(x_nodes, y_nodes, 'k.')
 
 A = zeros(n_nodes_y*n_nodes_x);
 B = zeros(n_nodes_y*n_nodes_x, 1);
@@ -24,25 +27,35 @@ j = 1; % Horizontal counter
 
 for k=1:n_nodes_y*n_nodes_x
     
-    
-    
-    
-    A(k,k) = A(k,k) - 2 *cd*(dx/dy + dy/dx);
-    A(k,k-1) = A(k,k-1) + cd*dy/dx;
-    A(k,k+1) = A(k,k+1) + cd*dy/dx;
-    A(k,k+n_nodes_x) = A(k,k+n_nodes_x) + cd*dx/dy;
-    A(k,k-n_nodes_x) = A(k,k-n_nodes_x) + cd*dx/dy;
-    
-    B(k) = source(centro_x(j), centro_y(i));
-    
-    
-    j = j+1;
-    if j == n_nodes_x
+    if j == 1 % Left Boundary
+    elseif j == n_nodes_x % Right Boundary
+
         j = 1;
-        i = i+1;
+        i = i+1; 
+     
+    elseif i == 1 % Lower Boundary
         
+    
+    elseif i == n_nodes_y % Upper Boundary
+        
+      k      
+ i 
+ j
+   
+        
+    else % Middle
+     A(k,k) = A(k,k) - 2 *cd*(dx/dy + dy/dx);
+        A(k,k-1) = A(k,k-1) + cd*dy/dx;
+        A(k,k+1) = A(k,k+1) + cd*dy/dx;
+        A(k,k+n_nodes_x) = A(k,k+n_nodes_x) + cd*dx/dy;
+        A(k,k-n_nodes_x) = A(k,k-n_nodes_x) + cd*dx/dy;
+
+        B(k) = source(centro_x(j), centro_y(i));
+        
+       
+        
+
     end
-    
-    
+j = j+1;
 end
 
