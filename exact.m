b8541df153e0c73924c86e51d57044120a57eba0
pmbm95 <38461119+pmbm95@users.noreply.github.com>
2018-07-29 16:47:57 +0100

    ddd

--- exact.m
+++ exact.m
@@ -1,4 +1,6 @@
 function u = exact(x,y,t)
     %u = 500.0 + 500.0 * ((sin(pi*x))^2 + (sin(pi*y))^2)/2;
-    u = 100*x^3*cos(2*pi*y)+10;
+    vx = 100*x^3*cos(2*pi*y)+10;
+    vy = 100*y^3*cos(2*pi*x)+20;
+    u = [vx vy]
 end
